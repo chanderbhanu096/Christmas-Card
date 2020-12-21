4e22bc970240cd4a70e692e5bf10d08c1dd578e8
Virginia Wilcox <wilcoxva@gmail.com>
2020-12-20 22:20:27 -0500

    added typewriter and rythm

--- christmas-card/src/components/Card.js
+++ christmas-card/src/components/Card.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
 
 function Card(props) {
-    console.log(props.location.state.name)
 
     return (
         <div className="card">
-            <p>Hello, {props.location.state.name}</p>
-            <button><Link to={{ pathname: '/lastpage', state: props.location.state.name }}>Next...</Link></button>
+            <div className="empty-space" />
+            <Typewriter
+                onInit={(typewriter) => {
+                    typewriter
+                    .typeString('<span class="typewriter">Twas the night before Christmas</span>')
+                    .start()
+                    .pauseFor(500)
+                    .typeString('<br><span class="typewriter">When all through the house</span>')
+                    .pauseFor(500)
+                    .typeString('<br><span class="typewriter">Not a creature was stirring</span>')
+                    .pauseFor(500)
+                    .typeString(`<br><span class="typewriter">Not even a ${props.location.state.name}!</span>`)
+                    .pauseFor(500)
+                    .deleteChars(props.location.state.name.length + 1)
+                    .typeString('<br><span class="typewriter">mouse!</span>')
+                    .pauseFor(1000)
+                    .typeString('<br><span class="typewriter">The stockings were hung</span>')
+                    .pauseFor(500)
+                    .typeString('<br><span class="typewriter">By the chimney with care</span>')
+                    .pauseFor(500)
+                    .typeString(`<br><span class="typewriter">In hopes that ${props.location.state.name}!</span>`)
+                    .pauseFor(500)
+                    .deleteChars(props.location.state.name.length + 1)
+                    .typeString('<br><span class="typewriter">Saint Nicholas</span>')
+                    .pauseFor(500)
+                    .typeString('<br><span class="typewriter">Soon would be there...<i>(End)</i></span>')
+                }}
+            />
+            <button className="card-btn"><Link to={{ pathname: '/lastpage', state: props.location.state.name }}>Next...</Link></button>
         </div>
     )
 };
--- christmas-card/src/components/LastPage.js
+++ christmas-card/src/components/LastPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import reindeer from '../images/1.png';
 
 function LastPage(props) {
     console.log("last page props", props.location.state)
     return (
         <div className="last-page">
             <p>Merry Christmas, {props.location.state}</p>
+            <img src={reindeer} />
         </div>
     )
 };
